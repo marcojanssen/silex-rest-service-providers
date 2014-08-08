d82ae931b4acdddac9184eeefc4d6cb4a1ecee7b
Marco Janssen <dev@marcojanssen.net>
2014-08-08 11:26:22 +0200

    Fixed running unit tests through the cli

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 use Doctrine\Common\Annotations\AnnotationRegistry;
-$loader = require_once __DIR__ . "/../vendor/autoload.php";
+$loader = require __DIR__ . "/../vendor/autoload.php";
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 $loader->add('MJanssen\\', __DIR__);
