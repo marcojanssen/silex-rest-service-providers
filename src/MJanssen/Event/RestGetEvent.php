bffa097afbb68a5b6a57f0fd75dbacc9c88ca2ba
Marco Janssen <dev@marcojanssen.net>
2014-11-12 11:17:07 +0100

    Added repository to event

--- src/MJanssen/Event/RestGetEvent.php
+++ src/MJanssen/Event/RestGetEvent.php
@@ -1,11 +1,20 @@
 <?php
 namespace MJanssen\Event;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\EventDispatcher\Event;
 
 class RestGetEvent extends Event
 {
+    /**
+     * @var object
+     */
     protected $entity;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repository;
 
     /**
      * @param $entity
@@ -21,6 +30,22 @@ class RestGetEvent extends Event
     public function getEntity()
     {
         return $this->entity;
+    }
+
+    /**
+     * @param EntityRepository $repository
+     */
+    public function setRepository(EntityRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @return EntityRepository
+     */
+    public function getRepository()
+    {
+        return $this->repository;
     }
 
 } 
--- test/MJanssen/Event/RestGetEventTest.php
+++ test/MJanssen/Event/RestGetEventTest.php
@@ -24,4 +24,28 @@ class RestGetEventTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function testSetRepository()
+    {
+        $repository = $this->getRepositoryMock();
+
+        $this->event->setRepository(
+            $repository
+        );
+
+        $this->assertSame(
+            $repository,
+            $this->event->getRepository()
+        );
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getRepositoryMock()
+    {
+        return $this->getMockBuilder('\Doctrine\ORM\EntityRepository')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+    }
+
 } 
